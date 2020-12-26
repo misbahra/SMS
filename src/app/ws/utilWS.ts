bd96406f6e98641652d8417d305f3f0f7f5c9059
misbahra <misbahra@gmail.com>
2020-12-26 16:16:34 +0400

    fixed gl

--- src/app/ws/utilWS.ts
+++ src/app/ws/utilWS.ts
@@ -106,28 +106,29 @@ export class utilWS {
 
 generatePdf(docDefinition : any , action:any){
 
-  (<any>pdfMake).tableLayouts = {
-    exampleLayout: {
-      hLineWidth: function (i, node) {
-        if (i === 0 || i === node.table.body.length) {
-          return 0;
-        }
-        return (i === node.table.headerRows) ? 2 : 1;
-      },
-      vLineWidth: function (i) {
-        return 0;
-      },
-      hLineColor: function (i) {
-        return i === 1 ? 'black' : '#aaa';
-      },
-      paddingLeft: function (i) {
-        return i === 0 ? 0 : 8;
-      },
-      paddingRight: function (i, node) {
-        return (i === node.table.widths.length - 1) ? 0 : 8;
-      }
-    }
-  };
+  // (<any>pdfMake).tableLayouts = {
+  //   exampleLayout: {
+  //     hLineWidth: function (i, node) {
+  //       if (i === 0 || i === node.table.body.length) {
+  //         return 0;
+  //       }
+  //       return (i === node.table.headerRows) ? 2 : 1;
+  //     },
+  //     vLineWidth: function (i) {
+  //       return 0;
+  //     },
+  //     hLineColor: function (i) {
+  //       return i === 1 ? 'black' : '#aaa';
+  //     },
+  //     paddingLeft: function (i) {
+  //       return i === 0 ? 0 : 8;
+  //     },
+  //     paddingRight: function (i, node) {
+  //       return (i === node.table.widths.length - 1) ? 0 : 8;
+  //     }
+  //   }
+  // };
+  
   
 
   if(action==='download'){    
